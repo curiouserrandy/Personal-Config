d5b37323d847235b24bf4e908c4650f1909e4c79
Randy Smith <rdsmith@google.com>
2010-05-21 15:53:05 -0400

    Accept large tags files.

--- Tools/chrome-dev/all.el
+++ Tools/chrome-dev/all.el
@@ -2,3 +2,6 @@
 ;; Try and setup C++ bindings properly for chrome
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
+
+(setq large-file-warning-threshold
+      (max large-file-warning-threshold (* 25 1024 1024))) ;For TAGS file.
