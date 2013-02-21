b6b69484f8bd7127b88446b00fd72722d892b1b1
Randy Smith <rdsmith@chromium.org>
2013-02-21 11:36:35 -0500

    Remove meta comment from cmd key.

--- OS/Darwin/all.el
+++ OS/Darwin/all.el
@@ -4,7 +4,6 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(mac-option-modifier 'meta)
- '(mac-command-modifier 'meta)
  '(safe-local-variable-values
    (quote ((outline-explode . ".*day")
 	   (outline-explode . "\\(Todo, active\\|Todo\\) *$")
