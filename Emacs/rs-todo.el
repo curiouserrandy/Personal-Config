18a0cba2ddf4a698c59510ad60c88d7463551cc8
Randy Smith <rdsmith@chromium.org>
2013-09-18 15:37:45 -0400

    Ignore -- not at beginning of line, and add note to forward/backward processing.

--- Emacs/rs-todo.el
+++ Emacs/rs-todo.el
@@ -183,10 +183,10 @@ is nil, (point-min) or (point-max) will be used instead."
 	;;; General case.  First create the regexp.
 	(setq search-re
 	      (concat "^"
-		      ;; Not done allows any other mark; don't scan for those
+		      ;; Not done allows any other mark (other than whitespce); don't scan for those
 		      (if (and (member t done) (member nil done)) ".?")
 		      (if (not (member nil done)) "X")
-		      (if (not (member t done)) "[^X]?")
+		      (if (not (member t done)) "[^X 	]?")
 
 		      ;; For each member of type
 		      ;;   I want to translate it to the prefix
@@ -220,7 +220,7 @@ is nil, (point-min) or (point-max) will be used instead."
   (let* ((myoutl (rstodo-get-outline-info (point)))
 	 (nextpiece
 	  (rstodo-get-related-piece-info
-	   (point) rel '("todo" "copy" "question")
+	   (point) rel '("todo" "copy" "question" "note")
 	   (if skip-done nil '(t nil))
 	   (if skip-wait nil '(t nil))
 	   (rstodo-outline-info-start myoutl)
