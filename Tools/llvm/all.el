38d5092b3ba1e51053c4303bf5afd4c598e4250b
Randy Smith <rdsmith@chromium.org>
2014-09-08 17:16:22 -0400

    Function to attach to an lldb process automatically.

--- Tools/llvm/all.el
+++ Tools/llvm/all.el
@@ -4,4 +4,8 @@ The directory containing FILE becomes the initial working directory
 and source-file directory of your debugger."
   t)
 
-
+(defun lldb-auto-wrap (binary proc_id)
+  (require 'gud-lldb)
+  (lldb (concat "lldb " binary))
+  (gud-call (concat "attach " (int-to-string proc_id)))
+  (gud-call "continue"))
