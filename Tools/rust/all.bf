0aa1c1f3a709e8f649bf38bd6daa63f4eeac08b5
Randy Smith <rdsmith@google.com>
2021-03-28 12:47:26 -0400

    Basic rust integration

---
+++ Tools/rust/all.bf
@@ -0,0 +1,1 @@
+suffix_path_with_dirlist PATH /Users/rdsmith/.cargo/bin
---
+++ Tools/rust/all.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path (concat (getenv "HOME") "/Checkouts/rust-mode/"))
+(autoload 'rust-mode "rust-mode" nil t)
